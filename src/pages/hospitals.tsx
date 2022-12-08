ab3c4855e954b11e2cd67abe73019cc9880c7463
Hans Schedel <hans.haugush@philips.com>
2022-12-08 12:32:00 -0300

    chore: improve hospitals

--- src/pages/hospitals.tsx
+++ src/pages/hospitals.tsx
@@ -2,32 +2,37 @@ import Link from 'next/link'
 import HospitalsMock from '@/mock/hospitals.json'
 import { searchLocation } from '@/utils/Functions'
 import { IHospital } from '@/utils/Interfaces'
+import { useGlobalConfig } from '@/context/GlobalConfig'
+import { HospitalCardsWrapper } from '@/styles/pages/Hospital'
+import Image from 'next/image'
 
 export default function Hospitals(): JSX.Element {
+  const { userLocation } = useGlobalConfig()
   const hospitals = HospitalsMock as IHospital[]
 
   return (
     <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      {hospitals.map(hospital => (
-        <>
-          <div>{hospital.name}</div>
-          <div>{hospital.address}</div>
-          <Link href={searchLocation(hospital.location)} target="_blank">
-            leve me para lá
-          </Link>
-          <br />
-          <br />
-          <br />
-        </>
-      ))}
+      <HospitalCardsWrapper>
+        {hospitals.map((hospital, index) => (
+          <section key={`hospital_${index}`}>
+            <Image
+              alt={`hospital_${hospital.name}`}
+              src={hospital.imgSrc}
+              height={200}
+              width={250}
+            />
+            <div>{hospital.name}</div>
+            <div>{hospital.address}</div>
+            <Link
+              href={searchLocation(hospital.location, userLocation)}
+              target="_blank"
+            >
+              leve me para lá
+            </Link>
+            <br />
+          </section>
+        ))}
+      </HospitalCardsWrapper>
     </>
   )
 }
